bb467b12e9e27b5862eb64319ea66a6406c6c210
hackathon IDF <hack@cs.colman.ac.il>
2022-03-07 18:14:37 +0200

    feat(detectLocation): detects location every 10 minutes and sends to server

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,90 @@ class PiranhaApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: Message(text: "Hello this is5 a message! khdkljldl;d", time: DateTime.now(), isCurrUser: false,),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Location location = Location();
+
+  LocationData? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLocation();
+  }
+
+  void fetchLocation() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    await location.enableBackgroundMode(enable: true);
+    await location.changeSettings(interval: 5000);
+
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      print(
+          'Latitude: ${currentLocation.latitude} Longitude: ${currentLocation.longitude}');
+      print(DateTime.fromMillisecondsSinceEpoch(currentLocation.time!.toInt()));
+      // sendLocationToServer();
+      setState(() {
+        _currentPosition = currentLocation;
+      });
+    });
+  }
+
+  void sendLocationToServer() async {
+    var url = Uri.parse('http://10.10.244.48:5000/location');
+    var response = await http.post(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'latitude': _currentPosition?.latitude,
+          'longitude': _currentPosition?.longitude,
+          'userId': 5,
+          'time': _currentPosition!.time!.toInt()
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
     );
   }
 }
