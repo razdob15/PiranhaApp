34791ab568023eae6c889237d10108ffec463e40
hackathon IDF <hack@cs.colman.ac.il>
2022-03-08 17:02:08 +0200

    fix(message): changed to current way of data

--- lib/screens/singlePageChat.dart
+++ lib/screens/singlePageChat.dart
@@ -9,18 +9,33 @@ class SinglePageChat extends StatefulWidget {
 }
 
 class _SinglePageChatState extends State<SinglePageChat> {
+
   List<Message> messages = [
-    Message(text: "Hello, Will", time: DateTime.now(), isCurrUser: false),
-    Message(text: "How have you been?", time: DateTime.now(), isCurrUser: false),
+    Message(
+        text: "Hello, Will",
+        time: DateTime.now(),
+        currUserId: "Kriss",
+        senderId: "Kriss"),
+    Message(
+        text: "How have you been?",
+        time: DateTime.now(),
+        currUserId: "Kriss",
+        senderId: "Kriss"),
     Message(
         text: "Hey Kriss, I am doing fine dude. wbu?",
         time: DateTime.now(),
-        isCurrUser: true),
-    Message(text: "ehhhh, doing OK.", time: DateTime.now(), isCurrUser: false),
+        currUserId: "Kriss",
+        senderId: "Will"),
+    Message(
+        text: "ehhhh, doing OK.",
+        time: DateTime.now(),
+        currUserId: "Kriss",
+        senderId: "Kriss"),
     Message(
         text: "Is there any thing wrong?",
         time: DateTime.now(),
-        isCurrUser: true),
+        currUserId: "Kriss",
+        senderId: "Will"),
   ];
 
   final myController = TextEditingController();
@@ -102,7 +117,8 @@ class _SinglePageChatState extends State<SinglePageChat> {
             itemBuilder: (context, index) {
               return Row(
                 children: [
-                  Flexible(child: SizedBox(
+                  Flexible(
+                      child: SizedBox(
                     height: 50,
                     child: messages[index],
                   ))
@@ -128,7 +144,7 @@ class _SinglePageChatState extends State<SinglePageChat> {
                           hintText: "Write message...",
                           hintStyle: TextStyle(color: Colors.black54),
                           border: InputBorder.none),
-                          controller: myController,
+                      controller: myController,
                     ),
                   ),
                   SizedBox(
