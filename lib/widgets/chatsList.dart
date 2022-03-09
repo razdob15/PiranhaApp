ad3a4ff890e5f3a43a5478e1da2ce4ba51fe62cd
hackathon IDF <hack@cs.colman.ac.il>
2022-03-09 09:38:37 +0200

    feat(chatPage):conect chat to message

--- lib/widgets/chatsList.dart
+++ lib/widgets/chatsList.dart
@@ -14,14 +14,16 @@ class Listtt extends StatefulWidget {
 
 class InputState extends State<Listtt> {
   
-  //final Map<int, ChatUsers> chatUsers = {111111111: new ChatUsers(sentFrom: 111111111, messageText: "Awesome Setup", time: DateTime.parse('1969-07-20 20:18:04Z')), 222222222: new ChatUsers(sentFrom: 111111111, messageText: "hiiiiii", time: DateTime.parse('1969-07-20 20:18:04Z'))};
+  final Map<int, List<ChatUsers>> chatUsers = {111111111: [ChatUsers(sentFrom: 111111111, messageText: "What's upp?", time: DateTime.parse('1969-07-20 20:18:04Z')), ChatUsers(sentFrom: 3333333333, messageText: "hello", time: DateTime.parse('1969-07-20 20:20:04Z'))]
+                                            , 222222222: [ChatUsers(sentFrom: 111111111, messageText: "hiiiiii", time: DateTime.parse('1969-07-20 06:18:04Z'))]
+                                            , 333333333: [ChatUsers(sentFrom: 111111111, messageText: "Idannnnnnnn", time: DateTime.parse('1969-07-20 06:18:04Z')), ChatUsers(sentFrom: 111111111, messageText: "Roniiiiii", time: DateTime.parse('1969-07-20 06:18:04Z')) ]};
   
-  List<ChatUsers> chatUsers = [
-    ChatUsers(sentFrom: 111111111, messageText: "Awesome Setup", time: DateTime.parse('1969-07-20 20:18:04Z')),
-    ChatUsers(sentFrom: 222222222, messageText: "That's Great", time: DateTime.parse('1969-07-20 16:31Z')),
-    ChatUsers(sentFrom: 333333333, messageText: "Hey where are you?", time: DateTime.parse('1969-07-20 10:18:04Z')),
-    ChatUsers(sentFrom: 444444444, messageText: "Busy! Call me in 20 mins", time: DateTime.parse('1969-07-20 06:20:04Z'))
-  ];
+  // List<ChatUsers> chatUsers = [
+  //   ChatUsers(sentFrom: 111111111, messageText: "Awesome Setup", time: DateTime.parse('1969-07-20 20:18:04Z')),
+  //   ChatUsers(sentFrom: 222222222, messageText: "That's Great", time: DateTime.parse('1969-07-20 16:31Z')),
+  //   ChatUsers(sentFrom: 333333333, messageText: "Hey where are you?", time: DateTime.parse('1969-07-20 10:18:04Z')),
+  //   ChatUsers(sentFrom: 444444444, messageText: "Busy! Call me in 20 mins", time: DateTime.parse('1969-07-20 06:20:04Z'))
+  // ];
   
 
   @override
@@ -32,9 +34,9 @@ class InputState extends State<Listtt> {
   padding: EdgeInsets.only(top: 16),
   itemBuilder: (context, index){
     return ConversationList(
-      sentFrom: chatUsers[index].sentFrom.toString(),
-      messageText: chatUsers[index].messageText,
-      time: chatUsers[index].time,
+      sentFrom: chatUsers.keys.elementAt(index).toString(),
+      messageText: chatUsers.values.elementAt(index)[0].messageText.toString(),
+      time: chatUsers.values.elementAt(index)[0].time,
     );
   },
 );
